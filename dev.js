9a28def15c4fe5add2df849ca789d8fbe643ca58
Pawel Psztyc <jarrodek@gmail.com>
2018-05-17 16:40:02 -0700

    Continue work on the builder.

---
+++ dev.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const builder = require('.');
+const workingDir = 'test/playground';
+
+builder({
+  local: 'test/api-console-5.0.0-preview.zip',
+  destination: workingDir,
+  api: 'test/api.raml',
+  verbose: true,
+  attributes: [
+    {
+      'append-headers': 'x-header: text'
+    },
+    'narrow',
+    'no-try-it'
+  ]
+});
