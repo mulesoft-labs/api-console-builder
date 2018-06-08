e8835d9649104cc3e3a92b664c6bfb6e6e3ba2a5
Pawel Psztyc <jarrodek@gmail.com>
2018-06-08 07:19:14 -0700

    Increasing build test timeout for embnedded version

--- test/embedded-build.test.js
+++ test/embedded-build.test.js
@@ -13,8 +13,7 @@ describe('Embedded with API - remote sources', () => {
   });
 
   it('Builds the console', function() {
-    console.log('CWD', process.cwd());
-    this.timeout(300000);
+    this.timeout(500000);
     return builder({
       tagName: '5.0.0-preview',
       destination: workingDir,
